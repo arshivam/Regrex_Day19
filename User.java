a23a993343caf5028fdd5ddc31a03d75b432c4c3
arshivam <shivam.armarkar007@gmail.com>
2022-02-22 13:51:11 +0530

    Added: program to validate email,userName,passWord,phoneNumber

---
+++ User.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.userregistration;
+
+public class User {
+	private final String firstName = " ";
+	private final String lastName = " ";
+	private final String email = " ";
+	private final String phoneNumber = " ";
+	private final String password = " ";
+	
+	
+}
